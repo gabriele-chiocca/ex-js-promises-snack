0ccb5b574892c04fb258114a773c587923c1bbee
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-14 11:54:11 +0200

    Snack 2

--- script.js
+++ script.js
@@ -15,20 +15,44 @@
 
 //Snack 1 BONUS
 
-function getPostId(id) {
+// function getPostId(id) {
+//   return new Promise((resolve, reject) => {
+//     fetch(`https://dummyjson.com/posts/${id}`)
+//       .then((response) => response.json())
+//       .then((post) => {
+//         fetch(`https://dummyjson.com/users/${post.userId}`)
+//           .then((response) => response.json())
+//           .then((user) => resolve({ post, user }))
+//           .catch((error) => reject(error));
+//       })
+//       .catch((error) => reject(error));
+//   });
+// }
+
+// getPostId(1)
+//   .then((messaggio) => console.log(messaggio))
+//   .catch((errore) => console.log(errore));
+
+//Snack 2
+
+function lanciaDado(scelta) {
   return new Promise((resolve, reject) => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((post) => {
-        fetch(`https://dummyjson.com/users/${post.userId}`)
-          .then((response) => response.json())
-          .then((user) => resolve({ post, user }))
-          .catch((error) => reject(error));
-      })
-      .catch((error) => reject(error));
+    console.log('Lancio il dado');
+
+    setTimeout(() => {
+      const valore = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+      const verificaValore = valore % 2 == 0 ? 'pari' : 'dispari';
+
+      if (scelta === verificaValore) {
+        resolve(`Hai vinto, è uscito il numero ${valore}`);
+      } else {
+        reject(`Hai perso, è uscito il numero ${valore}`);
+      }
+    }, 3000);
   });
 }
 
-getPostId(1)
-  .then((messaggio) => console.log(messaggio))
-  .catch((errore) => console.log(errore));
+lanciaDado('pari')
+  .then((risultato) => console.log(risultato))
+  .catch((errore) => console.error(errore));
