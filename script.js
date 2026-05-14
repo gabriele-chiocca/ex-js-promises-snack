424b8679036878b3fd796ec84635620942c42f66
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-14 11:38:44 +0200

    Snack 1 Bonus

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-function getPostTitle(id) {
+// Snack 1
+
+// function getPostTitle(id) {
+//   return new Promise((resolve, reject) => {
+//     fetch(`https://dummyjson.com/posts/${id}`)
+//       .then((response) => response.json())
+//       .then((obj) => resolve(obj.title))
+//       .catch((error) => reject(error));
+//   });
+// }
+
+// getPostTitle(1)
+//   .then((messaggio) => console.log(messaggio))
+//   .catch((errore) => console.error(errore));
+
+//Snack 1 BONUS
+
+function getPostId(id) {
   return new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
       .then((response) => response.json())
-      .then((obj) => resolve(obj.title))
+      .then((post) => {
+        fetch(`https://dummyjson.com/users/${post.userId}`)
+          .then((response) => response.json())
+          .then((user) => resolve({ post, user }))
+          .catch((error) => reject(error));
+      })
       .catch((error) => reject(error));
   });
 }
 
-getPostTitle(1)
+getPostId(1)
   .then((messaggio) => console.log(messaggio))
-  .catch((errore) => console.error(errore));
+  .catch((errore) => console.log(errore));
